785687284078288e6f989adabd48b4e78f45b1a5
egg651 <joebartlett94@gmail.com>
2013-10-15 15:52:45 +0100

    Initial Commit

---
+++ GmusicPlaylistExporter.py
@@ -0,0 +1,46 @@
+from gmusicapi import Webclient
+import os
+import getpass
+
+api = Webclient()
+
+api.__init__()
+
+while (api.login(raw_input('Email: '), getpass.getpass()) == False):
+	print 'Error, try again'
+
+i = 1
+for playlist in api.get_all_playlist_ids()['user']:
+	print str(i) + '. ' + playlist
+	i += 1
+
+while True:
+	chosen_number = int(raw_input('Choose a playlist to export (enter the number): '))
+	if chosen_number < i:
+		break
+	else:
+		print 'Error, try again'
+
+i = 1
+for playlist in api.get_all_playlist_ids()['user']:
+	if i == chosen_number:
+		chosen_id = api.get_all_playlist_ids()['user'][playlist][0]
+		chosen_name = playlist
+		break
+	else:
+		i += 1
+
+songs = []
+
+for song in api.get_playlist_songs(chosen_id):
+	songs.append(song['artist'] + ' - ' + song['album'] + ' - ' + song['name'])
+
+songs.sort(key=lambda s: s.lower())
+
+f = open(os.path.join(os.path.dirname(__file__), playlist + '.txt'), "w")
+for out in songs:
+ 	f.write(out.encode("utf-8") + "\n")
+
+api.logout()
+
+raw_input('Success! Press enter to exit.')
---
+++ readme.md
@@ -0,0 +1,10 @@
+Prerequisites
+=============
+
+* Python 2.7
+* [gmusicapi](https://github.com/simon-weber/Unofficial-Google-Music-API/blob/master/docs/source/index.rst)
+
+Usage
+=====
+
+Run GMusicPlaylistExporter.py and follow the prompts. A text file with the name of the playlist will be created in the same directory.
